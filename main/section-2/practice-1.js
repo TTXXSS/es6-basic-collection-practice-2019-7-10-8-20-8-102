0e59cdab46901b23ad5bf4b58e1004fda8ed5e46
TTXXSS <1374230455@qq.com>
2019-07-10 22:36:06 +0800

    complete counting the sum of the same element

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+//先将数组转化为Set数据类型，然后再转回数组类型
+  let copyNoSameCollection = Array.from(new Set(collection));
+  let collectionHasNumOfSameElement=[];
+  let obj=[];
+  copyNoSameCollection.forEach(i => {
+    let count=0;
+    collection.forEach(j => {
+      if(i===j) 
+      count++;
+  });
+  obj[i]=count;
+    
+  });
+  for(let i in obj){
+    collectionHasNumOfSameElement.push({key:i,count:obj[i]});
+  }
+  
+  return collectionHasNumOfSameElement;
 }
