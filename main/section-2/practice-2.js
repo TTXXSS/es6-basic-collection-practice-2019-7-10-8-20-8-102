af8392c1f89c90b239144c77b2252ac1f9ff50ad
TTXXSS <1374230455@qq.com>
2019-07-10 23:01:28 +0800

    complete count the same element includes 'd-5'

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let copyNoSameCollection = Array.from(new Set(collection));
+  let collectionHasNumOfSameElement = [];
+  let obj = [];
+  for(let i=0;i<copyNoSameCollection.length;i++){
+    let count = 0;
+  for(let j=0;j<collection.length;j++){
+    if (copyNoSameCollection[i] === "d-5") {
+      count += 5;
+      break;
+    }
+    else if (copyNoSameCollection[i] === collection[j])
+      count++;
+  }
+  if(copyNoSameCollection[i] ==="d-5")
+  obj[copyNoSameCollection[i].substring(0,1)] = count;
+  else
+  obj[copyNoSameCollection[i]] = count;
+}
+  
+  for (let i in obj) {
+    collectionHasNumOfSameElement.push({ key: i, count: obj[i] });
+  }
+
+  return collectionHasNumOfSameElement;
 }
